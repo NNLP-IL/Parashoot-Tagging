4da2ed068c68183299ea8b94134dbef395e0c4cc
Omri Keren <kerenom1994@gmail.com>
2022-01-20 12:19:38 +0200

    Added annotation code

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -9,7 +9,7 @@
       <p>
         cdQA-annotator a web-based annotator for closed-domain question answering datasets created for the
         <a
-          href="https://github.com/cdqa-suite"
+            href="https://github.com/cdqa-suite"
         >cdQA-suite</a> project.
       </p>
       <p>
@@ -22,16 +22,9 @@
       </p>
       <json-viewer :value="jsonData" :expand-depth="10" copyable></json-viewer>
       <br>
-      <div class="uploadBar">
-        <b-form-file
-          v-model="file"
-          :state="Boolean(file)"
-          placeholder="Upload a JSON file..."
-          accept=".json"
-        ></b-form-file>
-      </div>
       <br>
-      <b-button :size="''" :variant="'primary'" v-on:click="readFile()">Upload</b-button>
+      <b-button :size="''" :variant="'primary'" v-on:click="getRandomFile()">Start Annotation</b-button>
+      <br>
     </div>
   </div>
 </template>
@@ -41,7 +34,7 @@ import AnnotationsPage from "./AnnotationsPage.vue";
 
 export default {
   name: "HomePage",
-  data: function() {
+  data: function () {
     return {
       jsonData: {
         data: [
@@ -50,12 +43,12 @@ export default {
             paragraphs: [
               {
                 context:
-                  "Question answering (QA) is a computer science discipline within the fields of information retrieval and natural language processing (NLP), which is concerned with building systems that automatically answer questions posed by humans in a natural language.",
+                    "Question answering (QA) is a computer science discipline within the fields of information retrieval and natural language processing (NLP), which is concerned with building systems that automatically answer questions posed by humans in a natural language.",
                 qas: []
               },
               {
                 context:
-                  "A QA implementation, usually a computer program, may construct its answers by querying a structured database of knowledge or information, usually a knowledge base. More commonly, QA systems can pull answers from an unstructured collection of natural language documents.",
+                    "A QA implementation, usually a computer program, may construct its answers by querying a structured database of knowledge or information, usually a knowledge base. More commonly, QA systems can pull answers from an unstructured collection of natural language documents.",
                 qas: []
               }
             ]
@@ -67,18 +60,34 @@ export default {
         ]
       },
       fileUploaded: false,
-      file: null,
+      jsonID: null,
       json: null
     };
   },
   methods: {
-    readFile: function() {
-      var reader = new FileReader();
-      reader.onload = function(event) {
-        this.json = JSON.parse(event.target.result);
-        this.fileUploaded = true;
-      }.bind(this);
-      reader.readAsText(this.file);
+    pad: function (num, size) {
+      while (num.length < size) num = "0" + num;
+      return num;
+    },
+    readFile: function (jsonID) {
+      this.json = require("../json_resources/heb_squad-v1.1_" + this.pad(jsonID, 4) + ".json");
+      this.json.jsonID = jsonID;
+      this.fileUploaded = true;
+    },
+    getRandomInt: function (min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    getRandomFile: function () {
+      this.jsonID = this.getRandomInt(21, 2684).toString();
+      this.json = require("../json_resources/heb_squad-v1.1_" + this.pad(this.jsonID, 4) + ".json");
+      this.json.jsonID = this.jsonID;
+      this.fileUploaded = true;
+    },
+    state: function (jsonID) {
+      var intID = parseInt(jsonID)
+      return intID > 0 && intID < 1200;
     }
   },
   components: {
