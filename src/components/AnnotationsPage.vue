21eab63309237acb6d7c9ed353fc9ffcfb3efaad
Roialfassi <roialfassi@gmail.com>
2022-05-10 16:48:28 +0300

    deleted hotjar

--- src/components/AnnotationsPage.vue
+++ src/components/AnnotationsPage.vue
@@ -398,18 +398,6 @@ export default {
   }
 };
 </script>
-
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:2963075,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
